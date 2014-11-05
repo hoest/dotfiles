1699232594d91b0912070152eecfce69240211a8
Jelle de Jong <jelle@hoest.nl>
2014-11-05 22:06:01 +0100

    Shell script to install some essential software

---
+++ brew-cask-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+brew tap caskroom/versions
+
+brew cask install google-chrome
+brew cask install firefox
+brew cask install iterm2
+
+brew cask install macvim
+brew cask install sublime-text-dev
+
+brew cask install forklift
+brew cask install tower
+
+brew cask install transmit
+brew cask install onepassword
+
